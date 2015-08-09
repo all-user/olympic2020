93745ca4b505f8776d0a0c79d860a114be87da72
Keita Okamoto <ok@aaa.email.ne.jp>
2015-08-09 17:21:04 +0900

    define EmblemGroup constructor

--- src/js/EmblemGroup.js
+++ src/js/EmblemGroup.js
@@ -3,26 +3,37 @@ import Olympic2020 from './Olympic2020.js'
 class EmblemGroup {
 
     constructor(chars) {
-        let emblems;
-        switch (typeof chars) {
-            case 'string':
-                emblems = [].map.call(chars, c => new Olympic2020(c));
-                break;
-            case 'object':
-                if (Array.isArray(chars) && chars.every(o => o instanceof Olympic2020)) {
-                    emblems = chars;
-                } else {
-                    return false;
-                };
-                break;
-            default:
-                return false;
-        }
+        let emblems = _transfromToOlympic2020Array(chars);
 
-        this.emblems = emblems;
+        if (emblems) {
+            this.emblems = emblems;
+        } else {
+            throw new Error('EmblemGroup arguments expect string or array of Olympic2020.')
+        }
     }
 
-    map()
+    map(chars) {
+    }
 
 }
 
+function _transfromToOlympic2020Array(arg) { // (string | [Olympic2020]) => [Olympic2020] | false
+
+    let res;
+    switch (typeof arg) {
+        case 'string':
+            res = [].map.call(arg, c => new Olympic2020(c));
+            break;
+        case 'object':
+            if (Array.isArray(arg) && arg.every(o => o instanceof Olympic2020)) {
+                res = arg;
+            } else {
+                res = false;
+            };
+            break;
+        default:
+            res = false;
+    }
+
+    return res;
+}
